7f2428648c40bdae4eeb27ea9d37ffd219bfe99d
krishnapandey1504 <krishnap15042006@gmail.com>
2026-06-24 23:02:39 +0530

    Add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Hanken_Grotesk, Inter, JetBrains_Mono } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const hanken = Hanken_Grotesk({
   subsets: ["latin"],
@@ -23,7 +24,13 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${hanken.variable} ${inter.variable} ${mono.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+
+      {/* Replace with your GA Measurement ID */}
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </html>
   );
 }
